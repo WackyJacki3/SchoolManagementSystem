2bb1824aaf51d7dd4a8f9c155df9771d1fa0181b
mari0514 <111376774+mari0514@users.noreply.github.com>
2022-11-20 11:46:31 -0800

    modified the edit button

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,9 +1,20 @@
 // alert to comfirm the deletion of student
-const confirmDeletion = () => {
+const confirmStudentDeletion = () => {
     const response = confirm("Are you sure you want to remove this student?");
 
     if (response) {
         alert("Removing this student from the list...");
+    } else {
+        alert("Canceled deleting.");
+    }
+}
+
+// alert to comfirm the deletion of student
+const confirmTeacherDeletion = () => {
+    const response = confirm("Are you sure you want to remove this teacher?");
+
+    if (response) {
+        alert("Removing this teacher from the list...");
     } else {
         alert("Canceled deleting.");
     }
