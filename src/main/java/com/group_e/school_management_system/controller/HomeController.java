b2ce10d5f25081cd011e98d0c8aed9459cb11898
Jackie <kong123890@gmail.com>
2022-11-24 17:00:45 -0800

    Home Page Working Properly

--- src/main/java/com/group_e/school_management_system/controller/HomeController.java
+++ src/main/java/com/group_e/school_management_system/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.group_e.school_management_system.dao.iCourseRepository;
 import com.group_e.school_management_system.entity.Course;
@@ -21,5 +22,12 @@ public class HomeController {
         List<Course> courses = courseRepository.findAll();
         model.addAttribute("courses", courses);
         return "home/index";
+    }
+
+    @GetMapping("/showCourse/{id}")
+    public String displayCourse(@PathVariable("id") String courseId, Model model) {
+        Course course = courseRepository.findById(Long.parseLong(courseId));
+        model.addAttribute("course", course);
+        return "course/show-course";
     }
 }
